4340da209faaef804f45ad36c900b02d6db984c5
MdNihal05 <112348834+MdNihal05@users.noreply.github.com>
2023-12-07 11:19:43 +0530

    Create Look.cpp

---
+++ DISC_SHEDULING/Look.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define int long long
+#define all(x) x.begin(),x.end()
+using namespace std;
+
+int32_t main()
+{
+    int n, total_tracks, head; cin >> n >> head >> total_tracks;
+    vector<int>process(n);
+    for (int i = 0; i < n; i++) {
+        cin >> process[i];
+    }
+    sort(all(process));
+
+    int mn = *min_element(all(process));
+    int mx = *max_element(all(process));
+
+    cout << 2 * mx - head - mn << endl; // towards largest
+    cout << head + mx - 2 * mn; // towards smallest
+}
