84d39d1c9f6d130255fee37700645c5e6f0cecf8
MdNihal05 <112348834+MdNihal05@users.noreply.github.com>
2023-12-20 10:47:11 +0530

    Update FCFS.cpp

--- DISC_SHEDULING/FCFS.cpp
+++ DISC_SHEDULING/FCFS.cpp
@@ -6,10 +6,10 @@ int main()
     int n, head; cin >> n >> head;
     vector<int>processes(n);
     for (auto &process : processes) cin >> process;
-    int seek_time = 0, curr = head;
+    int seek_time = 0;
     for (auto &i : processes) {
-        seek_time += abs(curr - i);
-        curr = i;
+        seek_time += abs(head - i);
+        head = i;
     }
     cout << seek_time;
 }
