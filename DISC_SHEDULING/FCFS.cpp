aa06d50aa8b1a100dbd3fabdcea10698f497a332
MdNihal05 <112348834+MdNihal05@users.noreply.github.com>
2023-12-20 10:46:08 +0530

    Update FCFS.cpp

--- DISC_SHEDULING/FCFS.cpp
+++ DISC_SHEDULING/FCFS.cpp
@@ -3,13 +3,13 @@ using namespace std;
 //FCFS Disck sheduling
 int main()
 {
-	int n, head; cin >> n >> head;
-	vector<int>processes(n);
-	for (auto &process : processes) cin >> process;
-	int seek_time = 0, curr = head;
-	for (auto &i : processes) {
-		seek_time += abs(curr - i);
-		curr = i;
-	}
-	seek_time << ans;
+    int n, head; cin >> n >> head;
+    vector<int>processes(n);
+    for (auto &process : processes) cin >> process;
+    int seek_time = 0, curr = head;
+    for (auto &i : processes) {
+        seek_time += abs(curr - i);
+        curr = i;
+    }
+    cout << seek_time;
 }
