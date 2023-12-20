b56411a4a29f101d9ade3d958f2642ad00b5def8
MdNihal05 <112348834+MdNihal05@users.noreply.github.com>
2023-12-20 10:16:38 +0530

    Update LRU.cpp

--- Page-Replacement/LRU.cpp
+++ Page-Replacement/LRU.cpp
@@ -3,41 +3,40 @@
 #define all(x) x.begin(),x.end()
 using namespace std;
 
-
-
 int32_t main()
 {
     int n, frame; cin >> n >> frame;
     vector<int>process(n);
-    for (int i = 0; i < n; i++) {
-        cin >> process[i];
-    }
+    for (int i = 0; i < n; i++) cin >> process[i];
     set<int>s;
-    map<int, int>index;
+    deque<int>vis;
     int hit = 0, miss = 0;
     for (int i = 0; i < n; i++) {
         if (s.find(process[i]) != s.end()) {
             hit++;
-            index[process[i]] = i;
+            if (find(all(vis), process[i]) != vis.end()) {
+                vis.erase(find(all(vis), process[i]));
+            }
+            vis.push_back(process[i]);
+            for (auto &j : vis) cout << j << " ";
+            cout << "hit";
         } else {
             miss++;
             if (s.size() < frame) {
-                index[process[i]] = i;
+                vis.push_back(process[i]);
                 s.insert(process[i]);
             } else {
-                pair<int, int> F = {0, INT_MAX};
-                for (auto &i : index) {
-                    if (F.second > i.second) {
-                        F.second = i.second;
-                        F.first = i.first;
-                    }
-                }
-                index.erase(index.find(F.first));
-                s.erase(F.first);
-                index[process[i]] = i;
-                s.insert(process[i]);
+                s.erase(vis.front());
+                vis.pop_front();
+                vis.push_back(process[i]);
+                s.insert(vis.back());
             }
+            for (auto &j : vis) cout << j << " ";
+
+            cout << "miss";
         }
+        cout << endl;
     }
     cout << hit << " " << miss;
+
 }
